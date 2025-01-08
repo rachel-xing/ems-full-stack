65d41d42143c2659082644277e90a6b74905440f
Rachel Shing <xing0023@algonquinlive.com>
2025-01-08 15:52:08 -0500

    create Footer component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 import ListEmployeeComponent from "./components/ListEmployeeComponent.jsx";
 import HeaderComponent from "./components/HeaderComponent.jsx";
+import FooterComponent from "./components/FooterComponent.jsx";
 function App() {
   return (
     <>
         <HeaderComponent/>
         <ListEmployeeComponent/>
+        <FooterComponent/>
     </>
   )
 }
