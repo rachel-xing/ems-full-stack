075f11a97cd5db3dda675290f2683136564cae87
Rachel Shing <xing0023@algonquinlive.com>
2025-05-31 17:07:15 -0400

    Crate Header and Footer component

---
+++ ems-frontend/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+
+const Footer = () => {
+    return (
+        <footer className="footer bg-success-subtle mt-auto text-center py-3">
+            <span>All rights reserved 2025 by rachel</span>
+        </footer>
+
+    );
+};
+
+export default Footer;
---
+++ ems-frontend/src/components/Header.jsx
@@ -0,0 +1,61 @@
+import { useNavigate } from 'react-router-dom'
+
+// eslint-disable-next-line react/prop-types
+const Header = ({ onLogout }) => {
+    const navigator = useNavigate()
+
+    const handleLogout = () => {
+
+        if (window.confirm('确定要登出吗？')) {
+            onLogout()
+            navigator('/login')
+        }
+    }
+    return (
+        <header>
+            <nav className="navbar navbar-expand-lg bg-success-subtle">
+                <div className="container-fluid">
+                    <a className="navbar-brand" href="#">Trainer Management System</a>
+                    <button className="navbar-toggler" type="button"
+                            data-bs-toggle="collapse"
+                            data-bs-target="#navbarSupportedContent"
+                            aria-controls="navbarSupportedContent"
+                            aria-expanded="false"
+                            aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse"
+                         id="navbarSupportedContent">
+                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                            <li className="nav-item">
+                                <a className="nav-link"
+                                   aria-current="page" href="/ems-frontend/public">Home</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="/trainers">Trainers</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="/statistics">Statistics</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="/logout" onClick={ handleLogout }>Log Out</a>
+                            </li>
+
+                        </ul>
+                        <form className="d-flex" role="search">
+                            <input className="form-control me-2" type="search"
+                                   placeholder="Search" aria-label="Search"/>
+                            <button className="btn btn-outline-success"
+                                    type="submit">Search
+                            </button>
+                        </form>
+                    </div>
+                </div>
+            </nav>
+
+        </header>
+
+    )
+}
+
+export default Header
